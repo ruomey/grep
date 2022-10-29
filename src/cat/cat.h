a941ca699b56ad369b2e17c69efb8185cfe7eea7
Fanny Dante <fannydan@il-b3.msk.21-school.ru>
2022-10-29 18:39:29 +0300

    some code with flags -n -e

--- src/cat/cat.h
+++ src/cat/cat.h
@@ -5,8 +5,9 @@ int condition_args(char *argv);
 int change_flags(char *argv);
 void flag_e(char ch);
 void flag_b(char ch);
-void flag_n(char ch);
-void void_string(int argc, char *argv[ARG_MAX], char ch);
+void flag_n(char ch, int count);
+void void_string(char ch, int *flag_end_str, int *flag_str_void, int *count_void_str);
 void open_file(int argc, char *argv[ARG_MAX], int opiton);
+void count_str(char ch, int *count);
 
 #endif  // SRC_CAT_H_
--- src/cat/cat_function.c
+++ src/cat/cat_function.c
@@ -47,48 +47,57 @@ void flag_b(char ch){
     }
 }
 
-void flag_n(char ch){
-    int k = 1;
-    if (ch != EOF) {
-        printf("     %d  ", k);
+void flag_n(char ch, int count){
         if (ch == '\n') {
-            k++;
-            printf("\n     %d  ", k);
+            printf("\n     %d  ", count);
         } else {
             printf("%c", ch);
         }
     }
-}
-void void_string(int argc, char *argv[ARG_MAX], char ch){
-    int flag_end_str = 0, flag_str_void = 0;
-        int count_void_str = 0;
+void void_string(char ch, int *flag_end_str, int *flag_str_void, int *count_void_str){
         if (ch == '\n') {
-            if (!flag_str_void && !flag_end_str) {
-                flag_end_str = 1;
-            } else if (flag_end_str && !flag_str_void){
-                flag_str_void = 1;
-                count_void_str++;
-            } else if (flag_str_void && flag_end_str) {
-                count_void_str++;
+            if (!(*flag_str_void) && !(*flag_end_str)) {
+                *flag_end_str = 1;
+            } else if ((*flag_end_str) && !(*flag_str_void)){
+                *flag_str_void = 1;
+                (*count_void_str)++;
+            } else if ((*flag_str_void) && (*flag_end_str)) {
+                (*count_void_str)++;
             } 
         } else {
-            flag_str_void = 0;
-            flag_end_str = 0;
+            *flag_str_void = 0;
+            *flag_end_str = 0;
         }
-
 }
 void open_file(int argc, char *argv[ARG_MAX], int opiton){
-    for (int i = 1; i < argc; i++) {
+    int k = 1;
+    if (opiton == 3) {
+        printf("     %d  ", k);
+    }
+    for (int i = 2; i < argc; i++) {
         FILE *file = fopen(argv[i], "r");
         char ch = ' ';
-        if (file != NULL && ch != EOF) {
+        if (file != NULL) {
+            int flag_end_str = 0, flag_str_void = 0, count_void_str = 0;
             while ((ch = fgetc(file)) != EOF) {
+                void_string(ch, &flag_end_str, &flag_str_void, &count_void_str);
+                printf("\n %d",count_void_str);
                 if (opiton == 2) {
-                    ;
+                    
                 } else if (opiton == 1) {
                     flag_e(ch);
+                } else if (opiton == 3) {
+                    count_str(ch, &k);
+                    flag_n(ch, k);
                 }
              }
         }
+        fclose(file);
+    }
+}
+
+void count_str(char ch, int *count){
+    if (ch == '\n') {
+       (*count)++;
     }
 }
